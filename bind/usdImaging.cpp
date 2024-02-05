496b9f7cfd9a1a96ef0200a6803e2b6a6fd6bd8f
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-06 09:37:21 +1300

    Add more usdImaging methods, remove default boolean

--- bind/usdImaging.cpp
+++ bind/usdImaging.cpp
@@ -13,6 +13,10 @@ namespace bblext {
     void GLRenderParams_SetCullStyle(PXR_NS::UsdImagingGLRenderParams& params, PXR_NS::UsdImagingGLCullStyle cullStyle) {
         params.cullStyle = cullStyle;
     }
+
+    void GLRenderParams_SetColorCorrectionMode(PXR_NS::UsdImagingGLRenderParams& params, PXR_NS::TfToken colorCorrectionMode) {
+        params.colorCorrectionMode = colorCorrectionMode;
+    }
 }
 
 BBL_MODULE(usdImaging) {
@@ -22,7 +26,14 @@ BBL_MODULE(usdImaging) {
         .m(&PXR_NS::UsdImagingGLEngine::SetCameraState)
         .m(&PXR_NS::UsdImagingGLEngine::Render)
         .m(&PXR_NS::UsdImagingGLEngine::SetRendererSetting)
-        .m(&PXR_NS::UsdImagingGLEngine::TestIntersection);
+        .m(&PXR_NS::UsdImagingGLEngine::SetRendererAov)
+        .m(&PXR_NS::UsdImagingGLEngine::GetGPUEnabled)
+        .m(&PXR_NS::UsdImagingGLEngine::GetCurrentRendererId)
+        .m(&PXR_NS::UsdImagingGLEngine::SetRendererPlugin)
+        .m(&PXR_NS::UsdImagingGLEngine::GetRendererPlugins)
+        .m(&PXR_NS::UsdImagingGLEngine::GetRendererDisplayName)
+        .m(&PXR_NS::UsdImagingGLEngine::SetColorCorrectionSettings)
+        .m(&PXR_NS::UsdImagingGLEngine::TestIntersection)
     ;
 
     bbl::Class<PXR_NS::UsdImagingGLRenderParams>("GLRenderParams")
@@ -32,6 +43,7 @@ BBL_MODULE(usdImaging) {
 
     bbl::fn(&bblext::GLRenderParams_SetEnableLighting);
     bbl::fn(&bblext::GLRenderParams_SetCullStyle);
+    bbl::fn(&bblext::GLRenderParams_SetColorCorrectionMode);
 }
 
 
