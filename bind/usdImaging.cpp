856e645e87cea12db53ad6a2dd3b9ed155a2765f
Ashley Ruglys <ashley.ruglys@gmail.com>
2024-02-14 16:32:46 +1300

    Add hacky vulkan bindings

--- bind/usdImaging.cpp
+++ bind/usdImaging.cpp
@@ -4,6 +4,10 @@
 
 #include <pxr/usdImaging/usdImagingGL/engine.h>
 #include <pxr/usdImaging/usdImagingGL/renderParams.h>
+#include <pxr/imaging/hgiVulkan/hgi.h>
+#include <pxr/imaging/hgiVulkan/device.h>
+#include <pxr/imaging/hgiVulkan/instance.h>
+#include <pxr/imaging/hgiVulkan/texture.h>
 
 namespace bblext {
     void GLRenderParams_SetEnableLighting(PXR_NS::UsdImagingGLRenderParams& params, bool enableLighting) {
@@ -16,6 +20,32 @@ namespace bblext {
 
     void GLRenderParams_SetColorCorrectionMode(PXR_NS::UsdImagingGLRenderParams& params, PXR_NS::TfToken colorCorrectionMode) {
         params.colorCorrectionMode = colorCorrectionMode;
+    }
+
+    PXR_NS::UsdImagingGLEngine * GLEngine_from_vulkan(PXR_NS::HgiVulkan& hgi_vulkan) {
+        PXR_NS::Hgi* hgi = &hgi_vulkan;
+        PXR_NS::HdDriver driver {PXR_NS::HgiTokens->renderDriver, PXR_NS::VtValue(hgi)};
+        return new PXR_NS::UsdImagingGLEngine(driver);
+    }
+
+    uint64_t HgiVulkanInstance_GetVulkanInstance(PXR_NS::HgiVulkanInstance& instance) {
+        return (uint64_t)instance.GetVulkanInstance();
+    }
+
+    uint64_t HgiVulkanDevice_GetVulkanDevice(PXR_NS::HgiVulkanDevice& device) {
+        return (uint64_t)device.GetVulkanDevice();
+    }
+
+    uint64_t HgiVulkanDevice_GetVulkanPhysicalDevice(PXR_NS::HgiVulkanDevice& device) {
+        return (uint64_t)device.GetVulkanPhysicalDevice();
+    }
+
+    uint64_t HgiVulkanTexture_GetImage(PXR_NS::HgiVulkanTexture& texture) {
+        return (uint64_t)texture.GetImage();
+    }
+
+    PXR_NS::HgiVulkanTexture* get_vulkan_texture(PXR_NS::HgiTextureHandle& handle) {
+        return static_cast<PXR_NS::HgiVulkanTexture*>(handle.Get());
     }
 }
 
@@ -34,7 +64,31 @@ BBL_MODULE(usdImaging) {
         .m(&PXR_NS::UsdImagingGLEngine::GetRendererDisplayName)
         .m(&PXR_NS::UsdImagingGLEngine::SetColorCorrectionSettings)
         .m(&PXR_NS::UsdImagingGLEngine::TestIntersection)
+        .m(&PXR_NS::UsdImagingGLEngine::SetEnablePresentation)
+        .m(&PXR_NS::UsdImagingGLEngine::GetAovTexture)
     ;
+
+    bbl::Class<PXR_NS::HgiTextureHandle>("HgiTextureHandle");
+
+    bbl::Class<PXR_NS::HgiVulkanTexture>("HgiVulkanTexture")
+    ;
+
+    bbl::Class<PXR_NS::HgiVulkan>("HgiVulkan")
+        .ctor(bbl::Class<PXR_NS::HgiVulkan>::Ctor<>(), "new")
+        .m(&PXR_NS::HgiVulkan::GetVulkanInstance)
+        .m(&PXR_NS::HgiVulkan::GetPrimaryDevice)
+    ;
+
+ bbl::Class<PXR_NS::HgiVulkanInstance>("HgiVulkanInstance")
+    ;
+
+
+ bbl::Class<PXR_NS::HgiVulkanDevice>("HgiVulkanDevice")
+        .m(&PXR_NS::HgiVulkanDevice::GetVulkanDevice)
+        .m(&PXR_NS::HgiVulkanDevice::GetVulkanPhysicalDevice)
+        .m(&PXR_NS::HgiVulkanDevice::GetCommandQueue)
+    ;
+
 
     bbl::Class<PXR_NS::UsdImagingGLRenderParams>("GLRenderParams")
         .ctor(bbl::Class<PXR_NS::UsdImagingGLRenderParams>::Ctor<>(), "new");
@@ -44,6 +98,13 @@ BBL_MODULE(usdImaging) {
     bbl::fn(&bblext::GLRenderParams_SetEnableLighting);
     bbl::fn(&bblext::GLRenderParams_SetCullStyle);
     bbl::fn(&bblext::GLRenderParams_SetColorCorrectionMode);
+    bbl::fn(&bblext::GLEngine_from_vulkan);
+    bbl::fn(&bblext::HgiVulkanInstance_GetVulkanInstance);
+    bbl::fn(&bblext::HgiVulkanDevice_GetVulkanDevice);
+    bbl::fn(&bblext::HgiVulkanDevice_GetVulkanPhysicalDevice);
+
+    bbl::fn(&bblext::get_vulkan_texture);
+    bbl::fn(&bblext::HgiVulkanTexture_GetImage);
 }
 
 
